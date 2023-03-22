13e0d854aa4041fc706ef59836b6f2d265da5e1c
ngetpov <110336468+ngetpov@users.noreply.github.com>
2023-03-22 16:56:35 +0700

    Add files via upload

---
+++ contact_us.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="The official IceBerg Movies Torrents website. Download free  movies torrents in 720p, 1080p and 3D quality. The fastest downloads at the smallest size.">
+        <meta name="keywords" content=" IceBerg, 45 , -IBM45 , Free Movies , 1080p Free Movies , 3D Movies Free , Free Movie Torrent ">
+        <meta property="og:description" content="The official IceBerg Movies Torrents website. Download free  movies torrents in 720p, 1080p and 3D quality. The fastest downloads at the smallest size.">
+        <meta property="og:title" content="The official IceBerg Movies. Torrent Download - IceBerg" />
+        <meta property="og:image" content="http://localhost/project_midterm/images/image1.jpg" />
+        <meta property="og:url" content="http://ibm-online.net" />
+        
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+        <link rel="manifest" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+        <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <link href="./bootstrap5/bootstrap.min.css" rel="stylesheet">
+         <script src="./bootstrap5/bootstrap.bundle.min.js"></script>
+         <link href="./CSS/style.css" rel="stylesheet">
+        
+        <title>IceBergMovie</title>
+
+    </head>
+<body>
+    
+        <!-- Menu -->
+          <!-- NavBar  -->
+                    <!-- Menu -->
+                    <div class="navbar navbar-expand-md bg-dark navbar-dark">
+            <div class="container-fluid">
+            <a href="http://ibm-online.net" class="navbar-brand ">IceBerMovie</a>
+            <button class="humburger-icon navbar-toggler" data-bs-toggle="collapse" data-bs-target="#nav">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+        <div class="collapse navbar-collapse flex-row justify-content-end" id="#nav">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a href="index.php"class="nav-link active" >Home</a></li>
+                <li class="nav-item"><a href="contact_us.php" class="nav-link" >Contec Us</a></li>
+                <li class="nav-item"><a href="about_us.php" class="nav-link">About US</a></li>
+                <li class="nav-item"><a href="services.php" class="btn btn-outline-success">Servicse</a></li>
+                <i class="bi bi-star-fill"></i>
+            </ul>
+        </div>
+        </div>
+        </div>
+
+        <div class="container-fluid  bg-dark text-light">
+
+<div class="p-5 text-center bg-dark ">
+<h1 class="mb-3 text-light bg-dark">Download YTS YIFY movies: HD smallest size</h1>
+<p class="mb-3">Welcome to the official YTS.MX website. Here you can browse and download YIFY movies in excellent 720p, 1080p, 2160p 4K and 3D quality, all at the smallest file size. YTS Movies Torrents.</p>
+<a class="nav-link text-primary" href="https://www.facebook.com/khmer.way.12/"> 🕺 Keanu Reeves & 💋 Ana de Armas will star in 'John Wick' spin-off, 🌟 ‘BAL...</a>
+</div>
+</div>
+<div class="container-fluid Home-contend bg-dark text-light ">
+
+<div class="row text-center text-white-50">
+<h5 text-center >
+<i class="fa-solid fa-star"></i>
+    "popular downloads"
+    <a class="text-primary text-decoration-none" href="">more featured</a>
+</div>
+</h5>
+
+</div>
+<div class="contect_us-md ">
+    <head>
+        <h3 class="cotianer text-center">Where Can Contact</h3>
+    </head>
+
+        </div>
+<div class="container-md">
+ 
+ <table class="table table-bordered ">
+   <thead>
+     <tr>
+       <th scope="col">#</th>
+       <th scope="col">First Name</th>
+       <th scope="col">Last Name</th>
+       <th scope="col">Member Location</th>
+       <th scope="col">Phone Number</th>
+       <th scope="col">Other</th>
+     </tr>
+   </thead>
+   <tbody>
+     <tr>
+       <th scope="row">1</th>
+       <td>Mark</td>
+       <td>Otto</td>
+       <td>@mdo</td>
+       <td>0978205661</td>
+       <td>Active</td>
+     </tr>
+     <tr>
+       <th scope="row">2</th>
+       <td>Jacob</td>
+       <td>Thornton</td>
+       <td>@fat</td>
+     </tr>
+     <tr>
+       <th scope="row">3</th>
+       <td >Larry the Bird</td>
+       <td>@twitter</td>
+     </tr>
+   </tbody>
+ </table>
+ </div> 
+</body> 
+</html> 
---
+++ services.php
@@ -0,0 +1,75 @@
+<!Doctype html>
+<html>
+<head>
+    <title>Contact Us</title>
+    <meta name="keywords" content="Best Education university College"/>
+    <meta name="description" content="Our services has been serving since 1984. Please choose our services as your best partner." />
+    <meta charset="utf-8"/> 
+    <link rel="stylesheet" href="CSS/style.css">
+    <link href="bootstrap5/bootstrap.min.css" rel="stylesheet">
+    <script src="bootstrap5/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div class="mainDiv">
+        <!-- Menu -->
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="index.html">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="about_us.html">About Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="services.html">Services</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contact_us.html">Contact Us</a>
+            </li>
+        </ul>
+        <!-- End Menu -->
+        <!-- Banner -->
+        <div class="Banner">
+            Banner
+        </div>
+        <!-- End Banner -->
+        <!-- Body Content -->
+        <div class="BodyContent">
+            <div class="BodyContent_Top">
+                <div class="left">
+                    Left
+                </div>
+                <div class="center">
+                    <h1>Services</h1>
+                </div>
+                <div class="right">
+                    Right
+                </div>
+            </div>
+            <div class="BodyContent_Bottom">
+                <div class="pic" style="background-color:lightgray;">
+                    Pic1
+                </div>
+                <div class="pic" style="background-color:darkgray;">
+                    Pic2
+                </div>
+                <div class="pic" style="background-color:greenyellow;">
+                    Pic3
+                </div>
+                <div class="pic" style="background-color:whitesmoke;">
+                    Pic4
+                </div>
+                <div class="pic" style="background-color:royalblue;">
+                    Pic5
+                </div>
+            </div>
+        </div>
+        <!-- End Body Content -->
+        <!-- Footer -->
+        <div id="footer">
+            Footer
+        </div>
+        <!-- End Footer -->
+
+    </div>
+</body> 
+</html> 
